34b73838c1be70e383bf11d39496604562f614bb
Williamlarose <38011333+Williamlarose@users.noreply.github.com>
2021-05-21 12:08:38 -0400

    homepage,form,vaules

---
+++ src/Home.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+
+export default {
+    Homepage() {
+        return
+        (
+            <div>
+                <div>
+                    <h2>Welcome to Lam da la bomb pizza </h2>
+                    <img src='' />
+                </div>
+
+                <div>
+                    <p>Come on and enjoy are bomb pizza today </p>
+                </div>
+
+                <div><button>Order now </button></div>
+
+                <div>
+                    <h3>Other menu items</h3>
+                </div>
+
+                <div>
+                    <div><p>Burgers</p></div>
+                    <div><p>Pasta</p></div>
+                    <div><p>Hogoies</p></div>
+                </div>
+            </div>)
+    }
+}
